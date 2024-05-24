f3837f39c3a5da0d804c21c043bfebc44f2b84a7
Rizky Al Arief <rizkyalarief11@gmail.com>
2024-05-24 20:46:41 +0700

    fix: booking repository

--- src/repository/booking.repository.js
+++ src/repository/booking.repository.js
@@ -1,1 +1,57 @@
 const { Booking } = require("../../models")
+
+class bookingRepository {
+    
+    async getAllBooking() {
+        try {
+            const bookings = await Booking.findAll();
+            return bookings
+        } catch (error) {
+            throw error
+        }
+    }
+    async getBookingById(id) {
+        try {
+            const booking = await Booking.findByPk(id);
+            return booking;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async createBooking(dataBooking) {
+        try {
+            const newBooking = await Booking.create(dataBooking);
+            return newBooking;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateBooking(id, dataBooking) {
+        try {
+            const booking = await Booking.findByPk(id);
+
+            if (!booking) {
+                throw new Error('Booking not found');
+            }
+            await booking.update(dataBooking);
+            return booking;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteBooking(id) {
+        try {
+            const booking = await Booking.findByPk(id);
+
+            if (!booking) {
+                throw new Error('Booking not found');
+            }
+            await booking.destroy(dataBooking);
+            return booking;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
