c828cd659c0c967836fcdcf1edf4af1a602c6f66
Rizky Al Arief <rizkyalarief11@gmail.com>
2024-06-25 19:52:04 +0700

    try deploy

--- config/config.js
+++ config/config.js
@@ -2,24 +2,39 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DATABASE_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you connect to your DB with SSL
+        rejectUnauthorized: false // This line will fix the error
+      }
+    }
   },
   test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DATABASE_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT,
-  },
+    use_env_variable: 'POSTGRES_URL', // Use the URL directly for production
+    dialect: process.env.DATABASE_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
